5fcf419cf08638fe28ad385081ed7ebe925ecbce
Franziska Kunsmann <hi@kunsmann.eu>
2018-12-30 11:01:14 +0100

    add timer to periodically redraw lcd even if there is no activity

--- octoprint_hd44780/__init__.py
+++ octoprint_hd44780/__init__.py
@@ -46,6 +46,9 @@ class LCD_HD44780(octoprint.plugin.StartupPlugin,
 
         self._lcd_updating = False
 
+        timer = RepeatedTimer(30, self._lcd_update())
+        timer.start()
+
     def on_settings_initialized(self):
         self._initialize_lcd()
         self._printer.register_callback(self)
